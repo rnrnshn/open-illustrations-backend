ea1e6a8337eff16473ad07179a8bf8ff87bf94c9
rnrnshn <olymangue@gmail.com>
2024-05-20 02:48:31 +0000

    update

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
 import Fastify from "fastify";
 import axios from "axios";
 
-const app = Fastify({
-  logger: true,
-});
+const app = Fastify({ logger: true });
 
 // Use environment variables to store sensitive information
 const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 
+// Define the route handler for /get-images
 app.get("/get-images", async (req, res) => {
   try {
     const response = await axios.get(
@@ -28,7 +27,16 @@ app.get("/get-images", async (req, res) => {
   }
 });
 
+// Export the serverless function handler
 export default async function handler(req, res) {
-  await app.ready();
-  app.server.emit("request", req, res);
+  try {
+    // Fastify's ready() method waits for the server to be ready to handle requests
+    await app.ready();
+    // Emit the request event to the Fastify server
+    app.server.emit("request", req, res);
+  } catch (error) {
+    // Log and return any errors
+    console.error("Error handling request:", error);
+    res.status(500).send("Internal Server Error");
+  }
 }
